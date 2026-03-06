adc3d15f4a0626dbcb931d60f25a927bad2dd1a5
sriramskylimit <sriram.pandidurai@skylimitdigital.com>
2026-03-06 11:57:46 +0530

    endpoint changes

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 
   # E-commerce Web Platform Design
-
-  This is a code bundle for E-commerce Web Platform Design. The original project is available at https://www.figma.com/design/Sh91CmT7JctFiy76MS1PYo/E-commerce-Web-Platform-Design.
-
   ## Running the code
 
   Run `npm i` to install the dependencies.
--- src/app/types/order.ts
+++ src/app/types/order.ts
@@ -1,48 +1,3 @@
-// export type OrderStatus =
-//   | "pending"
-//   | "process"
-//   | "printing"
-//   | "packed"
-//   | "shipment"
-//   | "delivery";
-
-// export interface OrderItem {
-//   id: number;
-//   product_id: string;
-//   variant_id: string;
-//   quantity: number;
-//   price: number;
-//   total: number;
-// }
-
-// export interface OrderUser {
-//   id: string;
-//   username: string;
-//   email?: string | null;
-//   phone?: string | null;
-// }
-
-// export interface OrderAddress {
-//   address: string;
-//   city?: string | null;
-//   state?: string | null;
-//   country?: string | null;
-//   postal_code?: string | null;
-//   phone?: string | null;
-// }
-
-// export interface Order {
-//   id: string;
-//   status: OrderStatus;
-//   total_amount: number;
-//   created_at: string;
-//   updated_at: string;
-//   user: OrderUser;
-//   address?: OrderAddress | null;
-//   items: OrderItem[];
-// }
-
-
 export type OrderStatus =
   | "pending"
   | "process"
@@ -90,7 +45,7 @@ export interface Shipment {
   delivered_at?: string | null
 }
 
-/* ✅ UPDATE ORDER */
+/* UPDATE ORDER */
 export interface Order {
   id: string;
   status: OrderStatus;
@@ -101,5 +56,5 @@ export interface Order {
   address?: OrderAddress | null;
   items: OrderItem[];
 
-  shipment?: Shipment | null; // ✅ ADD THIS
+  shipment?: Shipment | null; 
 }
